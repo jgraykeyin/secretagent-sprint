9b2512337b7364f19c98caef156590780cb0e02b
jgraykeyin <justin.gray@keyin.com>
2021-06-07 08:58:06 -0230

    Single route response added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Secret agent stuff");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get('/', (request, response) => {
+    response.json({
+        info: 'Secret agent stuff'
+    })
+});
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});

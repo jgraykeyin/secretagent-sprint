bdddbd51dabbd3a82158eddf2c00f4753fb124c2
jgraykeyin <justin.gray@keyin.com>
2021-06-10 08:24:14 -0230

    Node now loads the frontend HTML

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./queries');
+let path = require('path')
 
 const app = express();
 const port = 3000;
@@ -12,34 +13,13 @@ app.use(
     })
 );
 
-// Allow VS code's Live Server speak to the Node server 
-// TODO: This will have to change once Node is setup to render HTML
-app.use(function (req, res, next) {
+// Make sure the public folder can be accessed for HTML & CSS
+app.use('/public', express.static('public'));
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.2.28:5500');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-// Main route
+// Main route to display the frontend
 app.get('/', (request, response) => {
-    response.json({
-        info: 'Secret agent stuff'
-    })
+    response.sendFile('index.html', { root: path.join(__dirname, './public/') });
 });
-
 
 // Create a new message
 app.post('/send', db.createMessage);
